aba76386d359b941e0672d4384573117479354bd
Jordan Tatum <jrtatum0133@eagle.fgcu.edu>
2022-12-06 19:54:10 -0500

    Master update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 
 function App() {
-  const CLIENT_ID = "88fb5309a15343db8f0e7d7891c98ea9"
-  const REDIRECT_URI = "http://localhost:3000"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
-
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spotify Networking Project</h1>
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login To Spotify</a>
+        <p>Enter a Song:</p>
+        <form>
+        <label>
+        <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   )
